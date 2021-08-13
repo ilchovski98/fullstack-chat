c9b09b76b01b8ff5de6aa74613ddfeab833117ba
ilchovski98 <ilchovski98@gmail.com>
2021-08-14 00:23:51 +0300

    Save progress on tutorial

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,18 +4,43 @@ import Home from './components/page-home/Home';
 import Register from './components/Register/Register';
 import Section from './components/global/Section/Section';
 import './assets/styles/main.scss';
-
+import React, {useState} from 'react';
 import { Route, Switch } from "react-router-dom";
 
 function App() {
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "admin123"
+  }
+
+  const [user, setUser] = useState({name: '', email: ''});
+  const [error, setError] = useState('');
+
+  const Login = details => {
+    console.log(details);
+    return '';
+  }
+
+  const Logout = () => {
+    console.log('logout');
+    return '';
+  }
+
   return (
     <div className="App">
       <Header />
       <Switch>
         <Route path="/login">
-          <Section className="section--small">
-            <Login />
-          </Section>
+          {(user.email !== '') ? (
+            <div className="welcome">
+              <h2>Welcome, <span>{user.name}</span></h2>
+              <button>Logout</button>
+            </div>
+          ) : (
+            <Section className="section--small">
+              <Login Login={Login} error={error} />
+            </Section>
+          )}
         </Route>
 
         <Route path="/register">
--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,26 +1,32 @@
 import Card from '../global/Card/Card';
+import React, {useState} from 'react';
 
-function Login() {
+function Login({Login, error}) {
+  const [details, setDetails] = useState({name: '', email: '', password: ''});
 
+  const submitHandler = e => {
+    e.preventDefault();
+    Login(details);
+  }
   return (
     <Card>
       <div className="form">
-          <form action="">
+          <form onSubmit={submitHandler}>
               <div className="form__head">
                   <h2 className="form__title">Login</h2>
               </div>
 
               <div className="form__body">
                   <div className="form__row">
-                      <label htmlFor="register-email" className="form__label">Email</label>
+                      <label htmlFor="login-email" className="form__label">Email</label>
 
-                      <input type="email" id="register-email" className="form__input" />
+                      <input type="email" id="login-email" className="form__input" />
                   </div>
 
                   <div className="form__row">
-                      <label htmlFor="register-first-name" className="form__label">Password</label>
+                      <label htmlFor="login-first-name" className="form__label">Password</label>
 
-                      <input type="password" id="register-password" className="form__input" />
+                      <input type="password" id="login-password" className="form__input" />
                   </div>
               </div>
 
